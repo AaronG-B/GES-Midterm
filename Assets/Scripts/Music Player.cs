ca7719e8c917763bf4a2f8697108dbb49ef6335b
Aaron G.B <aaron@gelblat-bronson.org>
2017-11-20 14:53:08 -0600

    Commit pre-upload (11/20)

---
+++ Assets/Scripts/Music Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+
+
+    AudioSource au;
+
+    // Use this for initialization
+	void Start () 
+    {
+        au = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
